54cc498db9207302f80656f227f70b744ea1d43e
WiaanB <whoppingwaki@gmail.com>
2022-07-06 22:39:50 +0200

    rewrote the structure of the project, to allow for some conditional treatment

--- curl.sh
+++ curl.sh
@@ -2,7 +2,29 @@
 
 printf "cURL examples and playground :)\n"
 
-USERS=$(curl -XGET https://jsonplaceholder.typicode.com/users)
+# Check if there were any arguments passed
+if [ $# -eq 0 ]
+then
+    echo "No options selected, please choose at least one from 'GET', 'POST', 'PUT', and 'DELETE'."
+    exit
+fi
 
-# print out the users pretty printed courtesy of jq
-echo $USERS | jq '.'
+# Loop through all the arguments provided
+for argument in $@
+do
+    if [ "$argument" == "GET" ]
+    then
+        # GET
+        echo "Executing GET..."
+        GET=$(curl -s -XGET https://jsonplaceholder.typicode.com/users)
+        echo $GET | jq '. | map({id: .id, name: .name, email: .email, company: .company.name})'
+    elif [ "$argument" == "POST" ]
+    then
+        # POST
+        echo "Executing POST..."
+        POST=$(curl -s -X POST https://jsonplaceholder.typicode.com/posts -H 'Content-Type: application/json' -d '{"title":"My new post", "body":"Lorem ipsom style beat", "userId": 1}')
+        echo $POST | jq '. | {"message":"success", "id": .id, "title": .title, "body": .body}'
+    else
+        echo "Wrong options selected, please choose at least one from 'GET', 'POST', 'PUT', and 'DELETE'."
+    fi
+done
